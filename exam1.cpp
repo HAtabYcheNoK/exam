756cd9e0ec2f5a915da4a571ddb9b02fb11dba82
HAtabYcheNoK <119106132+HAtabYcheNoK@users.noreply.github.com>
2022-12-13 22:36:44 +0200

    Add files via upload

---
+++ exam1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream> #incldue <cmath>
+int main()
+
+{
+    int A[10];
+    int i, j;
+    int t;
+    for(i=0; i<9; i++)
+        for(j=i; j>=0; j--)
+            if (A[j] < A[j + 1])
+            {
+                t = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = t;
+            }
+}
